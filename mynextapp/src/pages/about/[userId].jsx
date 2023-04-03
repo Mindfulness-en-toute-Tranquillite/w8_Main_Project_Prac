a088662d6df79abe5c73dac1be86805b908d13f8
Jay, Jaehyeok Yun <124968754+Mindfulness-en-toute-Tranquillite@users.noreply.github.com>
2023-04-03 19:30:47 +0900

    Nomad Coder - Next.JS 강의 #1. Framework overview #1.1 Library vs Framework #1.2 Pages #1.2 Static Pre Rendering

---
+++ mynextapp/src/pages/about/[userId].jsx
@@ -0,0 +1,15 @@
+
+import { useRouter } from 'next/router';
+import React from 'react'
+import Button from '@components/\bButton';
+
+function IdInAbout() {
+    const router = useRouter();
+    // const q = router.query;
+    // console.log(q);
+    return (
+    <Button></Button>
+    )
+}
+
+export default IdInAbout;
