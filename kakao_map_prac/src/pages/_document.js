50a24518b4d091d348ed6241c5153c65b2e081b1
Jay, Jaehyeok Yun <124968754+Mindfulness-en-toute-Tranquillite@users.noreply.github.com>
2023-04-04 15:08:41 +0900

    .env파일 생성 및 적용, 맵 키워드,주소 검색 추가.

--- kakao_map_prac/src/pages/_document.js
+++ kakao_map_prac/src/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
       <body>
         <Main />
         <NextScript />
+        <Script
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBILC_KAKAOMAP_KEY}&libraries=services,clusterer&autoload=false`}
+          strategy="beforeInteractive"
+        />
       </body>
     </Html>
-  )
+  );
 }
