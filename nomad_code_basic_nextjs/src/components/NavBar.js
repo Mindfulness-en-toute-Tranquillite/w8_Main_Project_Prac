8428b650501bcbb17c492a242eeba01c41a5a692
Jay, Jaehyeok Yun <124968754+Mindfulness-en-toute-Tranquillite@users.noreply.github.com>
2023-04-09 04:40:48 +0900

    #1.3 Routing + useRouter.

--- nomad_code_basic_nextjs/src/components/NavBar.js
+++ nomad_code_basic_nextjs/src/components/NavBar.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar(){
+    const router = useRouter();
+    // console.log(router)
     return <nav>
         <Link 
-        style={{color: "red"}}
-        href="/">Home</Link>
-        <Link href="/about">About Us</Link>
+        style={{color: router.pathname === "/" ? "red" : "blue"}}
+        href="/">Home
+        </Link>
+
+        <Link 
+        style={{color: router.pathname === "/about" ? "red" : "blue"}}
+        href="/about">About Us</Link>
     </nav>
 }
