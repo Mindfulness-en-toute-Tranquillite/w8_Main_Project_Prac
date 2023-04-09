74cdfd5ad078cd34b94775d11068ae2634a28233
Jay, Jaehyeok Yun <124968754+Mindfulness-en-toute-Tranquillite@users.noreply.github.com>
2023-04-09 15:37:10 +0900

    #1.4 CSS Modules 3분48초.

--- nomad_code_basic_nextjs/src/components/NavBar.js
+++ nomad_code_basic_nextjs/src/components/NavBar.js
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css"
 
 export default function NavBar(){
     const router = useRouter();
     // console.log(router)
-    return <nav>
+    return <nav className={styles.nav}>
         <Link 
-        style={{color: router.pathname === "/" ? "red" : "blue"}}
+        // style={{color: router.pathname === "/" ? "red" : "blue"}}
         href="/">Home
         </Link>
 
         <Link 
-        style={{color: router.pathname === "/about" ? "red" : "blue"}}
+        // style={{color: router.pathname === "/about" ? "red" : "blue"}}
         href="/about">About Us</Link>
     </nav>
 }
