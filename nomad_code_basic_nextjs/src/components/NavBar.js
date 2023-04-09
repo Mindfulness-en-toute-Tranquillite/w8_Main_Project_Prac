b70af84633bedae878888dcfd897ad21e56f6b60
Jay, Jaehyeok Yun <124968754+Mindfulness-en-toute-Tranquillite@users.noreply.github.com>
2023-04-09 16:19:44 +0900

    #1.5 Styles JSX.

--- nomad_code_basic_nextjs/src/components/NavBar.js
+++ nomad_code_basic_nextjs/src/components/NavBar.js
@@ -1,18 +1,38 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css"
 
 export default function NavBar(){
     const router = useRouter();
     // console.log(router)
     return <nav>
-        <Link 
-        className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}
-        href="/">Home
+        <Link legacyBehavior
+        href="/">
+        <a
+        className={router.pathname === "/" ? "active":""}
+        >Home</a>
         </Link>
 
-        <Link 
-        className={[styles.link, router.pathname === "/about" ? styles.active : ""].join(" ")}
-        href="/about">About Us</Link>
+        <Link legacyBehavior
+        href="/about">
+        <a
+        className={router.pathname === "/about" ? "active":""}
+        >About Us</a>
+        </Link>
+        <style jsx>{`
+        nav {
+            background-color: tomato;
+        }
+        a {
+            text-decoration: none;
+        }
+        .active {
+            color: yellow;
+        }
+
+        `}</style>
     </nav>
 } 
+
+        /* .active {
+            color: ${props.color};
+        } */
