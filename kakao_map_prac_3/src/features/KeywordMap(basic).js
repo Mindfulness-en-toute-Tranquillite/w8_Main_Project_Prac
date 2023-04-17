09edbb3754fcd4998f46e859247c895213c39dd3
Jay, Jaehyeok Yun <124968754+Mindfulness-en-toute-Tranquillite@users.noreply.github.com>
2023-04-17 16:59:00 +0900

    검색 목록 구현 완료 및 여러번 검색 오류 해결, styled-components 적용 및 세팅 완료, 오류 해결. 마커 오류 해결.

--- kakao_map_prac_3/src/features/KeywordMap(basic).js
+++ kakao_map_prac_3/src/features/KeywordMap(basic).js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
+import styled from 'styled-components';
 
-const MapContainer = ({ searchPlace }) => {
-    useEffect(() => {
-
+const MapContainer = ({ props }) => {
     const { kakao } = window;
     const map = new kakao.maps.Map(document.getElementById('myMap'), 
     {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        center: new kakao.maps.LatLng(37.5546788388674, 126.970606917394),
         level: 3
     });
     const ps = new kakao.maps.services.Places();
@@ -19,34 +18,35 @@ const MapContainer = ({ searchPlace }) => {
     // ps.keywordSearch(searchPlace, placesSearchCB);
     // console.log("ps->",searchPlace)
 
-    // kakao 제공
-    searchPlaces();
-    // 내가 적용한 것
-    const searchForm = document.getElementById("submit_btn");
-            searchForm?.addEventListener("click", function (e) {
-            e.preventDefault();
-            searchPlaces();
-    });
-
-    function searchPlaces() {
-        const keyword = document.getElementById("keyword")?.value;
-      
-        if (!keyword?.replace(/^\s+|\s+$/g, "")) {
-    alert("키워드를 입력해주세요!");
-      
-        return false;
+    const searchForm = document.getElementById('submit_btn');
+    if (searchForm) {
+        searchForm.addEventListener('click', function (e) {
+        e.preventDefault();
+        searchPlaces();
+        });
     }
-      
+    
+    // kakao 제공
+    useEffect(() => {
+        searchPlaces();
+    }, []);
+    function searchPlaces() {
+        const keyword = document.getElementById("keyword").value;
+        if (!keyword?.replace(/^\s+|\s+$/g, "")) {
+            alert("키워드를 입력해주세요!");
+            return false;
+    }
+    // 장소검색 객체를 통해 키워드로 장소검색을 요청
     ps.keywordSearch(keyword, placesSearchCB);
     }
     // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
     function placesSearchCB(data, status, pagination) {
         if (status === kakao.maps.services.Status.OK) {
-                    // 검색 목록과 마커를 표출합니다
-        displayPlaces(data);
-        // 페이지 목록 보여주는 displayPagination() 추가
-        displayPagination(pagination);
-        // setPlaces(data)
+            // 검색 목록과 마커를 표출합니다
+            displayPlaces(data);
+            // 페이지 목록 보여주는 displayPagination() 추가
+            displayPagination(pagination);
+            // setPlaces(data)
 
         const bounds = new kakao.maps.LatLngBounds();
         for (let i = 0; i < data.length; i++) {
@@ -54,6 +54,7 @@ const MapContainer = ({ searchPlace }) => {
             bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
         }
         map.setBounds(bounds)
+
         // 정상적으로 검색이 완료됐으면
         }   else if (status === kakao.maps.services.Status.ZERO_RESULT) {
             alert('검색 결과가 존재하지 않습니다.');
@@ -65,7 +66,6 @@ const MapContainer = ({ searchPlace }) => {
     }
     // 검색 결과 목록과 마커를 표출하는 함수입니다
     function displayPlaces(places) {
-
         const listEl = document.getElementById('placesList');
         const menuEl = document.getElementById('menu_wrap');
         const fragment = document.createDocumentFragment();
@@ -117,7 +117,7 @@ const MapContainer = ({ searchPlace }) => {
                 };
                 itemEl.addEventListener("click", function (e) {
                     displayInfowindow(marker, title);
-                    props.setAddress(places[i]);
+                    // props.setAddress(places[i]);
                     map.panTo(placePosition);
                 });
             })(marker, places[i].place_name);
@@ -127,8 +127,10 @@ const MapContainer = ({ searchPlace }) => {
         // 검색결과 항목들을 검색결과 목록 Element에 추가합니다
         if (listEl) {
             listEl.appendChild(fragment);
-            menuEl.scrollTop = 0;
+            if (menuEl !== null) { // menuEl이 null이 아닐 때 scrollTop 속성을 설정합니다.
+                menuEl.scrollTop = 0;
             }
+        }
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
         map.setBounds(bounds);
     }
@@ -201,7 +203,7 @@ const MapContainer = ({ searchPlace }) => {
     // 검색결과 목록의 자식 Element를 제거하는 함수입니다
     function removeAllChildNods(el) {
         if (el && el.hasChildNodes()) {
-            while (el.childNodes) {
+            while (el.childNodes.length > 0) { // changed condition to check if there are still child nodes
                 el.removeChild(el.lastChild);
             }
         }
@@ -213,14 +215,15 @@ const MapContainer = ({ searchPlace }) => {
         const fragment = document.createDocumentFragment();
         // 기존에 추가된 페이지 번호 삭제
         while (paginationEl?.hasChildNodes()) {
+            paginationEl.lastChild &&
             paginationEl.removeChild(paginationEl.lastChild)
         }
-  
+
         for (let i = 1; i <= pagination.last; i++) {
             const el = document.createElement('a')
             el.href = '#'
             el.innerHTML = i
-  
+
             if (i === pagination.current) {
             el.className = 'on';
             } else {
@@ -230,18 +233,20 @@ const MapContainer = ({ searchPlace }) => {
                 }
             })(i)
             }
-  
+
             fragment.appendChild(el)
         }
-        paginationEl.appendChild(fragment)
+        if (paginationEl !== null) { // paginationEl이 null이 아닐 때 appendChild 메소드를 호출합니다.
+            paginationEl.appendChild(fragment);
+        }
     }
-      
     function displayMarker(place) {
+        // 마커를 생성하고 지도에 표시합니다
         let marker = new kakao.maps.Marker({
         map: map,
         position: new kakao.maps.LatLng(place.y, place.x)
         });
-  
+        // 마커에 클릭이벤트를 등록합니다
         kakao.maps.event.addListener(marker, 'click', function(mouseEvent) {
             props.setAddress(place);
             infowindow.setContent(`<span>${place.place_name}</span>`);
@@ -250,13 +255,13 @@ const MapContainer = ({ searchPlace }) => {
             map.panTo(moveLatLon);
         });
     }
-}, []);
     return null;
     };
+    
     const MapContainerWrapper2 = () => {
         const [inputText, setInputText] = useState("");
         const [place, setPlace] = useState("");
-  
+
         const onChange = (e) => {
         setInputText(e.target.value);
         };
@@ -271,30 +276,65 @@ const MapContainer = ({ searchPlace }) => {
     
     return (
         <>
-            <section className='map_wrap'>
+            <MapSection className='map_wrap'>
                 <Map
+                    id='myMap'
                     center={{
-                        lat: 37.566826,
-                        lng: 126.9786567
+                        lat: 37.5546788388674,
+                        lng: 126.970606917394
                     }}
                     level={3}
-                    id='myMap'
                     style={{
                         width: "100%",
-                        height: "350px"
-                    }}>
+                        height: "550px",
+                        position: "absolute",
+                        overflow: "hidden",
+                        borderRadius: "20px"
+                    }}
+                    >
                 <MapContainer searchPlace={place} />
                 </Map>
-                <div id="menuDiv">
-                    <form className="inputForm" onSubmit={handleSubmit}>
-                        <input
-                            placeholder="Search Place..."
-                            onChange={onChange}
-                            id="keyword"
-                            value={inputText} />
-                        <button id="submit_btn" type="submit">검색</button>
-                    </form>
-                    <ul id="placesList">
+                <div 
+                id="menuDiv"
+                >
+                    <div 
+                    id="menu_wrap"
+                    >
+                        <div>
+                            <div 
+                            id="map_title"
+                            >
+                                <div>만나잔</div>
+                            </div>
+                        
+                            <form 
+                            className="inputForm" 
+                            id="form"
+                            onSubmit={handleSubmit}
+                            >
+                                <input
+                                    placeholder="Search Place..."
+                                    onChange={onChange}
+                                    id="keyword"
+                                    value={inputText} 
+                                    />
+                                <button 
+                                id="submit_btn" 
+                                type="submit"
+                                >검색
+                                </button>                           
+                            </form>
+                        </div>
+
+                        <ul 
+                    id="placesList"
+                    ></ul>
+                    <div id="pagination"></div>
+                    </div>
+                </div>
+
+
+
                         {Places.map((item, i) => (
                         <div key={i} style={{ marginTop: '20px' }}>
                             <span>{i + 1}</span>
@@ -312,12 +352,129 @@ const MapContainer = ({ searchPlace }) => {
                             </div>
                         </div>
                         ))}
-                        <div id="pagination"></div>
-                    </ul>
-                </div>
-            </section>
+                        
+                    
+                
+            </MapSection>
         </>
     );
 };
 
 export default MapContainerWrapper2;
+
+const MapSection = styled.div`
+  display: flex;
+  #map {
+    width: 920px;
+    height: 600px;
+    position: absolute;
+    overflow: hidden;
+    border-radius: 20px;
+    /* z-index: "1"; */
+  }
+  #menuDiv {
+    display: flex;
+    position: relative;
+    z-index: 2;
+    font-size: 12px;
+  }
+
+  #menu_wrap {
+    position: relative;
+    width: 400px;
+    height: 600px;
+    border-radius: 20px;
+    overflow-y: auto;
+    background: rgba(255, 255, 255, 0.7);
+  }
+
+  #map_title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 10px;
+  }
+
+  #form {
+    display: flex;
+    justify-content: space-between;
+    padding: 0px 15px 10px 15px;
+  }
+
+  #keyword {
+    width: 100%;
+    border: none;
+    outline: none;
+  }
+
+  #submit_btn {
+    background-color: #ff6e30;
+    border: none;
+    outline: none;
+  }
+
+  #placesList h5 {
+    color: #ff6e30;
+  }
+
+  #placesList li {
+    list-style: square;
+  }
+  #placesList .item {
+    border-bottom: 1px solid #888;
+    overflow: hidden;
+    cursor: pointer;
+  }
+
+  #placesList .item .info {
+    padding: 10px 0 10px 5px;
+  }
+
+  #placesList .item span {
+    display: block;
+    margin-top: 4px;
+  }
+  #placesList .info .gray {
+    color: #8a8a8a;
+  }
+
+  #placesList .info .tel {
+    color: #009900;
+  }
+  #btnDiv {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  #pagination {
+    margin: 10px auto;
+    text-align: center;
+  }
+  #pagination a {
+    display: inline-block;
+    margin-right: 10px;
+    color: #7b7b7b;
+  }
+  #pagination .on {
+    font-weight: bold;
+    cursor: default;
+    color: #ff6e30;
+  }
+  #btnOn {
+    height: 600px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  #searchBtn {
+    width: 20px;
+    padding: 0px;
+    height: 70px;
+    background-color: #ffa230;
+    border: none;
+    outline: none;
+  }
+`;
