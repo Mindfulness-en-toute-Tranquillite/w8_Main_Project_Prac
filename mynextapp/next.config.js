4c44b5e5ea215897c70a049bd3b77c094dadaf0d
Jay <tvghoy@gmail.com>
2023-07-14 19:12:04 +0900

    Nomad Coder - Next.JS 강의 #2 Practice Project #2.2 Redirect and Rewrite (2)

--- mynextapp/next.config.js
+++ mynextapp/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 // const API_KEY = "d-------";
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 const nextConfig = {
   reactStrictMode: true,
