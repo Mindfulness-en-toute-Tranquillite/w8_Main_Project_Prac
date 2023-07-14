15a6be0faeff4adf095dec915601de0ced17b611
Jay <tvghoy@gmail.com>
2023-07-14 18:13:00 +0900

    Nomad Coder - Next.JS 강의 #2 Practice Project #2.1 Fetching Data(2)

--- mynextapp/src/pages/index.js
+++ mynextapp/src/pages/index.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Head from "next/head";
 import Seo from '@components/Seo';
 
+const API_KEY = "d6a4bc48f364c0414fdafb71f660b184";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, [])
   return (
     <div>
       <Seo title="Home"/>
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key= {movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   )
 }
