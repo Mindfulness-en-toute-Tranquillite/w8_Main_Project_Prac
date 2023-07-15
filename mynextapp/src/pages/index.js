b488c4168ca7a00b478278b451b09a0cc34ad3c8
Jay <tvghoy@gmail.com>
2023-07-15 23:48:24 +0900

    Nomad Coder - Next.JS 강의 #2 Practice Project #2.2 Redirect and Rewrite -> api인증문제 및  401에러로 데이터 불러올 수 없어 코드 다시 수정

--- mynextapp/src/pages/index.js
+++ mynextapp/src/pages/index.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import Head from "next/head";
 import Seo from '@components/Seo';
 
-const API_KEY = "d6a4bc48f364c0414fdafb71f660b184";
-
 export default function Home() {
+
+  const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
       const { results } = await (
         await fetch(
-        `/api/movies`
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
         )
       ).json();
       setMovies(results);
