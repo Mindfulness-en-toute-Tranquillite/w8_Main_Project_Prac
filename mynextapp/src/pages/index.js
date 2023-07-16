ab03fe64d83208a32a2bc41238a2a6000ad0e8f1
Jay <tvghoy@gmail.com>
2023-07-16 15:50:47 +0900

    Nomad Coder - Next.JS 강의 #2 Practice Project #2.6 Movie Detail (1)

--- mynextapp/src/pages/index.js
+++ mynextapp/src/pages/index.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import Seo from '@components/Seo';
+import Link from 'next/link';
 
 export default function Home({ results }) {
   return (
     <div className="container">
     <Seo title="Home" />
     {results?.map((movie) => (
-      <div className="movie" key={movie.id}>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-        <h4>{movie.original_title}</h4>
-      </div>
+      <Link legacyBehavior href={`/movies/${movie.id}`} key={movie.id}>
+        <a>
+          <div className="movie">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+            <h4>{movie.original_title}</h4>
+          </div>
+        </a>
+      </Link>
     ))}
     <style jsx>{`
       .container {
